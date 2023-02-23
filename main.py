c1ed01f693ad9c922b52ee9834c4274da0fc15b6
eddie-30 <eddieahianyo@gmail.com>
2023-02-23 17:18:03 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name= "Edinam Ahianyo"
+phone_number= 507597909
+age= 18
+print(name)
+print(phone_number)
+print(age)
+names_of_students= ["Kwadjo","Ama","Yaw"]
+Second_student= names_of_students[1]
+print(Second_student)
+names_of_students.append ("Micheal")
+print(names_of_students)
